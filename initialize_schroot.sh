4a68163dc12bb3b694024ab4fc9260ce13c5344e
Gogs <gogs@fake.local>
2022-02-12 02:15:54 -0600

    copy over services and protocols before running schroot_setup_32bit_build_environment.sh

--- initialize_schroot.sh
+++ initialize_schroot.sh
@@ -4,10 +4,11 @@ sudo mkdir -p /srv/chroot/ubuntu_i386
 sudo debootstrap --variant=buildd --arch=i386 focal /srv/chroot/ubuntu_i386 http://archive.ubuntu.com/ubuntu/
 
 sudo cp /etc/apt/sources.list /srv/chroot/ubuntu_i386/etc/apt/
-sudo schroot -c ubuntu_i386 -u root $HOME/wine-dirs/schroot_setup_32bit_build_environment.sh
 
 sudo cp services /srv/chroot/ubuntu_i386/etc
 sudo cp protocols /srv/chroot/ubuntu_i386/etc
 
 sudo chmod root:root /srv/chroot/ubuntu_i386/etc/services
 sudo chmod root:root /srv/chroot/ubuntu_i386/etc/protocols
+
+sudo schroot -c ubuntu_i386 -u root $HOME/wine-dirs/schroot_setup_32bit_build_environment.sh
