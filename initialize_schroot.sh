d064f29f1b5d12d5faf290557c9ab2554dbdb0e4
Gogs <gogs@fake.local>
2022-02-12 01:37:40 -0600

    added protocols and services for schroot to prevent prompting

--- initialize_schroot.sh
+++ initialize_schroot.sh
@@ -6,3 +6,5 @@ sudo debootstrap --variant=buildd --arch=i386 focal /srv/chroot/ubuntu_i386 http
 sudo cp /etc/apt/sources.list /srv/chroot/ubuntu_i386/etc/apt/
 sudo schroot -c ubuntu_i386 -u root $HOME/wine-dirs/schroot_setup_32bit_build_environment.sh
 
+sudo cp services /srv/chroot/ubuntu_i386/etc
+sudo cp protocols /srv/chroot/ubuntu_i386/etc
