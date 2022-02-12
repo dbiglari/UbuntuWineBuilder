319147c1401c11da8518e5940289f6edb132cc60
Gogs <gogs@fake.local>
2022-02-11 23:21:39 -0600

    added quotes to update_sources.sh

--- update_sources.sh
+++ update_sources.sh
@@ -5,20 +5,19 @@ SOURCE1="deb-src http://archive.ubuntu.com/ubuntu focal main restricted universe
 SOURCE2="deb-src http://archive.ubuntu.com/ubuntu focal-updates main restricted universe multiverse"
 SOURCE3="deb-src http://security.ubuntu.com/ubuntu focal-security main restricted universe multiverse"
 
-if [ $(cat /etc/apt/sources.list | grep $SOURCE1 | wc -l) -ne 1 ]
+if [ $(cat /etc/apt/sources.list | grep "$SOURCE1" | wc -l) -ne 1 ]
 then
     sudo echo $SOURCE1 >> /etc/apt/sources.list
 fi
 
-if [ $(cat /etc/apt/sources.list | grep $SOURCE2 | wc -l) -ne 1 ]
+if [ $(cat /etc/apt/sources.list | grep "$SOURCE2" | wc -l) -ne 1 ]
 then
     sudo echo $SOURCE2 >> /etc/apt/sources.list
 fi
 
-if [ $(cat /etc/apt/sources.list | grep $SOURCE3 | wc -l) -ne 1 ]
+if [ $(cat /etc/apt/sources.list | grep "$SOURCE3" | wc -l) -ne 1 ]
 then
     sudo echo $SOURCE3 >> /etc/apt/sources.list
 fi
 
 sudo apt-get update
-
