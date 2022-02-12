e65f08b6caf3878a702e72610fc49df7df2cd40a
Gogs <gogs@fake.local>
2022-02-12 11:48:18 -0600

    added libx11-6 and libxext6 to dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -12,5 +12,6 @@ sudo apt-get -y install gcc make perl
 sudo apt-get -y install libxext-dev
 sudo apt-get -y install gcc-multilib g++-multilib
 sudo apt-get -y install mingw-w64-x86-64-dev mingw-w64-i686-dev mingw-w64-tools mingw-w64-common mingw-w64
+sudo apt-get -y install libx11-6 libxext6 libx11-6:i386 libxext6:i386
 sudo apt-get -y build-dep wine
 
--- schroot_setup_32bit_build_environment.sh
+++ schroot_setup_32bit_build_environment.sh
@@ -10,5 +10,6 @@ apt-get -y install build-essential
 apt-get -y install pkg-config flex bison libx11-dev libfreetype-dev xutils-dev gettext libxrender-dev libopengl-dev libgnutls28-dev libasound2-dev libusb-1.0-0-dev mingw-w64 samba-dev libpcap-dev
 apt-get -y install git nano
 apt-get -y install command-not-found sudo
+apt-get -y install libx11-6 libxext6
 apt-get -y build-dep wine
 
