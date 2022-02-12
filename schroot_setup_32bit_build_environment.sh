fc923b5f1a66fe7af0a5bd43a78afe83683783a6
Gogs <gogs@fake.local>
2022-02-12 01:14:42 -0600

    Removed deprecated --force-yes and replaced with -y

--- schroot_setup_32bit_build_environment.sh
+++ schroot_setup_32bit_build_environment.sh
@@ -3,8 +3,8 @@
 apt-get update
 
 # install wine build dependencies, using --force-yes to stop any prompts
-apt-get --force-yes install build-essential
-apt-get --force-yes install pkg-config flex bison libx11-dev libfreetype-dev xutils-dev gettext libxrender-dev libopengl-dev libgnutls28-dev libasound2-dev libusb-1.0-0-dev mingw-w64 samba-dev libpcap-dev
-apt-get --force-yes install git nano
-apt-get --force-yes install command-not-found sudo
-apt-get --force-yes build-dep wine
+apt-get -y install build-essential
+apt-get -y install pkg-config flex bison libx11-dev libfreetype-dev xutils-dev gettext libxrender-dev libopengl-dev libgnutls28-dev libasound2-dev libusb-1.0-0-dev mingw-w64 samba-dev libpcap-dev
+apt-get -y install git nano
+apt-get -y install command-not-found sudo
+apt-get -y build-dep wine
