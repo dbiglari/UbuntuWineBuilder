03129511cf7587f6efdeb9411482e848f90712da
Gogs <gogs@fake.local>
2022-02-12 00:02:18 -0600

    Added gcc-multilib to install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -10,5 +10,6 @@ sudo apt-get -y install build-essential
 sudo apt-get -y install pkg-config flex bison libx11-dev libfreetype-dev xutils-dev gettext libgnutls28-dev libasound2-dev libusb-1.0-0-dev samba-dev libpcap-dev libcapi20-dev libcapi20-dev:i386 libfreetype-dev:i386 git
 sudo apt-get -y install gcc make perl
 sudo apt-get -y install libxext-dev
+sudo apt-get -y install gcc-multilib g++-multilib
 sudo apt-get -y build-dep wine
 
